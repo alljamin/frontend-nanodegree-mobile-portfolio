784d0b4d1a41c0b4635650ca8f8cc359c7eaad25
Aleksandr Ljamin <aleksandr.lmn@gmail.com>
2017-07-11 12:00:15 +0930

    introduced imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,10 @@ var gulp = require('gulp'),
     useref = require('gulp-useref'),
     gulpIf = require('gulp-if'),
     del = require('del');
-    critical = require('critical').stream;
+    // critical = require('critical').stream;
+    imagemin = require('gulp-imagemin');
 
-gulp.task('clean-docs', function(){
+gulp.task('clean', function(){
     del.sync('./docs/**/**')
 })
 
@@ -14,7 +15,7 @@ gulp.task('useref', function(){
     gulp.src('./src/index.html')
         .pipe(useref())
         .pipe(gulpIf('*.js', uglify()))
-        // .pipe(gulpIf('*.css',minifyCSS()))
+        .pipe(gulpIf('*.css',minifyCSS()))
         .pipe(gulp.dest('./docs'))
     gulp.src('./src/projects/*.html')
         .pipe(useref())
@@ -28,35 +29,27 @@ gulp.task('useref', function(){
         .pipe(gulp.dest('./docs/projects/pizza'))
 });
 
-gulp.task('copy-files', function(){
-    gulp.src('./src/assets/img/**/*.+(png|ico|jpg|gif|jpeg)')
-        .pipe(gulp.dest('./docs/assets/img'))
-    gulp.src('./src/projects/pizza/assets/img/**/*.+(png|ico|jpg|gif|jpeg)')
-        .pipe(gulp.dest('./docs/projects/pizza/assets/img'))
+gulp.task('images', function () {
+    gulp.src('src/assets/img/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('docs/assets/img'))
+    gulp.src('src/projects/pizza/assets/img/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('docs/projects/pizza/assets/img'))
 });
 
+//have a problem with getting critical to work
 // gulp.task('critical', function () {
-//     critical.generate({
-//         inline: true,
-//         base: 'docs/',
-//         src: 'index.html',
-//         dest: 'docs/index.html',
-//         minify: true,
-//         width: 320,
-//         height: 480
-//     });
+//     gulp.src('docs/*.html')
+//         .pipe(critical({base: 'docs/', inline: true, css: 'docs/css/main.css'}))
+//         .pipe(gulp.dest('docs'));
 // });
-
-gulp.task('critical', function () {
-    gulp.src('docs/*.html')
-        .pipe(critical({base: 'docs/', inline: true, css: 'docs/css/main.css'}))
-        .pipe(gulp.dest('docs'));
-});
 
 gulp.task('watch', function(){
     gulp.watch(['./src/*.html','./src/projects/*.html','./src/projects/pizza/*.html'], ['useref'])
     gulp.watch(['./src/assets/css/*.css','./src/projects/pizza/assets/css/*.css'], ['useref'])
     gulp.watch(['./src/assets/js/*.js','./src/projects/pizza/assets/js/*.js'], ['useref'])
+    gulp.watch(['./src/assets/img/*','./src/projects/pizza/assets/img/*'], ['images'])
 });
 
-gulp.task('default', ['clean-docs', 'useref', 'copy-files', 'watch']);
+gulp.task('default', ['clean', 'useref', 'images', 'watch']);
