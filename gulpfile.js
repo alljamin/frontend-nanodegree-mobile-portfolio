65c4f6beb505d2f1a3922bc864e6142fe315e82c
Aleksandr Ljamin <aleksandr.lmn@gmail.com>
2017-07-12 19:08:50 +0930

    introduced autoprefixer and critical

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,58 @@ var gulp = require('gulp'),
     useref = require('gulp-useref'),
     gulpIf = require('gulp-if'),
     del = require('del');
-    // critical = require('critical').stream;
+    critical = require('critical');
     imagemin = require('gulp-imagemin');
+    autoprefixer = require('gulp-autoprefixer');
+
 
 gulp.task('clean', function(){
     del.sync('./docs/**/**')
 })
 
+//all optimised files go to docs
 gulp.task('useref', function(){
     gulp.src('./src/index.html')
         .pipe(useref())
         .pipe(gulpIf('*.js', uglify()))
-        .pipe(gulpIf('*.css',minifyCSS()))
+        .pipe(gulpIf('*.css', autoprefixer({browsers: ['last 40 versions']}))) // so that we can support IE9
+        .pipe(gulpIf('*.css', minifyCSS()))
         .pipe(gulp.dest('./docs'))
     gulp.src('./src/projects/*.html')
         .pipe(useref())
         .pipe(gulpIf('*.js', uglify()))
-        .pipe(gulpIf('*.css',minifyCSS()))
+        .pipe(gulpIf('*.css', autoprefixer({browsers: ['last 40 versions']})))
+        .pipe(gulpIf('*.css', minifyCSS()))
         .pipe(gulp.dest('./docs/projects'))
     gulp.src('./src/projects/pizza/pizza.html')
         .pipe(useref())
         .pipe(gulpIf('*.js', uglify()))
-        .pipe(gulpIf('*.css',minifyCSS()))
+        .pipe(gulpIf('*.css', autoprefixer({browsers: ['last 40 versions']})))
+        .pipe(gulpIf('*.css', minifyCSS()))
         .pipe(gulp.dest('./docs/projects/pizza'))
+});
+
+// critical will overwrite .html files in docs
+gulp.task('critical', function () {
+    critical.generate({
+        inline: true,
+        base: 'docs/',
+        src: 'index.html', 
+        dest: 'index.html', //instead of creating new file it will override the existing one
+        width: 320,
+        height: 480,
+        minify: true
+    });
+    critical.generate({
+        inline: true,
+        base: 'docs/projects/pizza',
+        src: 'pizza.html',
+        dest: 'pizza.html',
+        width: 320,
+        height: 480,
+        minify: true
+    });
+    // no easy way to generate critical css for multiple different html files at once
 });
 
 gulp.task('images', function () {
@@ -38,13 +67,6 @@ gulp.task('images', function () {
 		.pipe(gulp.dest('docs/projects/pizza/assets/img'))
 });
 
-//have a problem with getting critical to work
-// gulp.task('critical', function () {
-//     gulp.src('docs/*.html')
-//         .pipe(critical({base: 'docs/', inline: true, css: 'docs/css/main.css'}))
-//         .pipe(gulp.dest('docs'));
-// });
-
 gulp.task('watch', function(){
     gulp.watch(['./src/*.html','./src/projects/*.html','./src/projects/pizza/*.html'], ['useref'])
     gulp.watch(['./src/assets/css/*.css','./src/projects/pizza/assets/css/*.css'], ['useref'])
@@ -52,4 +74,4 @@ gulp.task('watch', function(){
     gulp.watch(['./src/assets/img/*','./src/projects/pizza/assets/img/*'], ['images'])
 });
 
-gulp.task('default', ['clean', 'useref', 'images', 'watch']);
+gulp.task('default', ['clean', 'critical', 'images', 'watch']);
